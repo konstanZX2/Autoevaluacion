cc02c9012c0ac24afc67c20251fff156baaf8196
konstanZX2 <zxgenerationzx@gmail.com>
2022-05-09 20:42:06 +0200

    Give me a break, it's 2am.  But it works now.

--- src/CargadorDeFicheroDeTexto.java
+++ src/CargadorDeFicheroDeTexto.java
@@ -6,14 +6,16 @@ public class CargadorDeFicheroDeTexto {
     BufferedReader bufferedReader;
 
     public List<String[]> lectorDeTextos() throws ExcepcionPropia {
-        List<String[]> palabras = new LinkedList<>();
+
 
         try {
             bufferedReader = new BufferedReader(new FileReader("Resources/texto.txt"));
             String linea = bufferedReader.readLine();
             while (linea != null) {
-                String[] palabra = linea.split(" ");
-                palabras.add(palabra);
+                String[] palabras = linea.split(" ");
+                for (String palabra: palabras ) {
+                    System.out.println(palabra);
+                }
                 linea = bufferedReader.readLine();
 
             }
@@ -26,6 +28,6 @@ public class CargadorDeFicheroDeTexto {
 
 
 
-        return palabras;
+        return new LinkedList<>();
     }
 }
