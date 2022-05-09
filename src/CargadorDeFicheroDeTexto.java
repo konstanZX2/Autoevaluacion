605e9af9ab98519ea5e495cd3e275811f87e998c
konstanZX2 <zxgenerationzx@gmail.com>
2022-05-09 20:49:00 +0200

    This is why the cat shouldn't sit on my keyboard.

--- src/CargadorDeFicheroDeTexto.java
+++ src/CargadorDeFicheroDeTexto.java
@@ -11,14 +11,20 @@ public class CargadorDeFicheroDeTexto {
         try {
             bufferedReader = new BufferedReader(new FileReader("Resources/texto.txt"));
             String linea = bufferedReader.readLine();
+            int contadorDePalabras=0;
+            int numeroDeLineas=0;
             while (linea != null) {
                 String[] palabras = linea.split(" ");
+                numeroDeLineas++;
                 for (String palabra: palabras ) {
+                    contadorDePalabras++;
                     System.out.println(palabra);
                 }
                 linea = bufferedReader.readLine();
 
             }
+            System.out.println(contadorDePalabras);
+            System.out.println(numeroDeLineas);
             bufferedReader.close();
 
         } catch (IOException excepcionPropia) {
